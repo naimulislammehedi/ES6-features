3a21a15ff890ca3f345acb7e613800450f165576
Mehedi <mehedi.interface@gmail.com>
2026-06-05 11:59:04 +0600

    Destructuring Assignment: Extract values from arrays or objects.

---
+++ destructuring_assignment.js
@@ -0,0 +1,9 @@
+// Destructuring Assignment: Extract values from arrays or objects. 
+// Array Destructuring 
+const [a, b] = [10, 20]; 
+console.log(a, b)
+
+// Object Destructuring 
+const person = {name: "Alice", age: 22}; 
+const {name, age} = person; 
+console.log(person)
